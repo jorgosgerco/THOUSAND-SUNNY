64b5e2724cc917670c555203a7c8852e2d48bbe6
𝐉𝐎𝐑𝐆𝐎𝐒 ⚜️ <91697863+jorgosgerco@users.noreply.github.com>
2025-08-16 10:38:29 +0200

    FIX

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,20 +5,16 @@ const { REST, Routes } = require('discord.js');
 require("dotenv").config();
 
 const commands = [];
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            commands.push(command.data.toJSON());
-        } else {
-            console.log(`[WARNING] Komanda te ${filePath} i mungon 'data' ose 'execute'.`);
-        }
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+    } else {
+        console.log(`[WARNING] Komanda te ${filePath} i mungon 'data' ose 'execute'.`);
     }
 }
 
