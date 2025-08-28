3273f8903941bf1f04d85815ea13b0bd3e70af2a
𝐉𝐎𝐑𝐆𝐎𝐒 ⚜️ <91697863+jorgosgerco@users.noreply.github.com>
2025-08-28 20:02:26 +0200

    PENALTY STUCK

--- events/penalty.js
+++ events/penalty.js
@@ -2,26 +2,21 @@
 const { Events } = require('discord.js');
 const { applyPenalties } = require('../bounty.js');
 
-let hasExecuted = false;
-
 module.exports = {
     name: Events.ClientReady,
     once: true,
     async execute(client) {
-        if (hasExecuted) return;
-        hasExecuted = true;
-
-        const checkInterval = 24 * 60 * 60 * 1000; // 24 ore ne milisekonda
+        const checkInterval = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
 
         const checkForPenalties = async () => {
             console.log('Duke kontrolluar per perdorues inaktive...');
             await applyPenalties();
         };
 
-        // Nis kontrollin per here te pare
-        checkForPenalties();
-
-        // Vendos nje interval per te kontrolluar cdo 24 ore
+        // This will run the function for the first time after 24 hours,
+        // and then continue to run it every 24 hours thereafter.
         setInterval(checkForPenalties, checkInterval);
+
+        console.log('Penalty check scheduled to run every 24 hours.');
     }
 };
